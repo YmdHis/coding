5d1045a25f027cdde537f39e798ea3131fa27b5d
chenxinhong <v_xhonchen@tencent.com>
2021-08-02 18:08:13 +0800

    es6 let

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-let foo = () => {console.log('foo')};
+//git clone https://github.com/zhoutk/Tetris
+for (let i=0; i<5;i++){
+  console.log('i',i)
+}
+
+var j 
+for (j=0; j<5;j++){
+  let n = j
+  console.log('j',j)
+}
